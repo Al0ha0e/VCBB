53be193aa898dde5d31d28e18f3d0957c6f9f9d9
Al0ha0e <1919270783@qq.com>
2019-08-25 20:08:13 +0800

    DAG test

--- client/prepos_test2.py
+++ client/prepos_test2.py
@@ -6,14 +6,66 @@ cnps = []
 
 
 def imp1(id):
-    return [id*3, id*3+1, id*3+2]
+    return [0]
 
 
 def omp1(id):
-    return [id*4+9, id*4+10, id*4+11, id*4+12]
+    return [1, 2]
 
 
 code1 = """
+t = input[0]
+output=[t+b'1919810',t+b'114514']
 """
 
-cnps.append(preprocessor.computeNode(2, 3, 4, imp1, omp1, code1))
+cnps.append(preprocessor.computeNode(1, 1, 2, imp1, omp1, code1))
+
+
+def imp2(id):
+    return [2]
+
+
+def omp2(id):
+    return [3, 4]
+
+
+code2 = """
+t = input[0]
+output=[t+b'1919810',t+b'114514']
+"""
+
+cnps.append(preprocessor.computeNode(1, 1, 2, imp2, omp2, code2))
+
+
+def imp3(id):
+    return [1, 3]
+
+
+def omp3(id):
+    return [5]
+
+
+code3 = """
+output=[input[0]+input[1]]
+"""
+
+cnps.append(preprocessor.computeNode(1, 2, 1, imp3, omp3, code3))
+
+
+def imp4(id):
+    return [5, 4]
+
+
+def omp4(id):
+    return [6]
+
+
+code4 = """
+output=[input[0]+input[1]]
+"""
+
+cnps.append(preprocessor.computeNode(1, 2, 1, imp4, omp4, code4))
+
+data = [str(114514).encode('utf-8')]
+
+client.commitScheduleGraph(7, cnps, data)
